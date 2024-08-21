0b548671fb326483a0f3e13e34a176be08d447c3
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-08-21 15:01:17 +0200

    fix: version typo in check

--- src/check.ts
+++ src/check.ts
@@ -1,6 +1,6 @@
 const tracker = await fetch("https://tracker.vendetta.rocks/tracker/index");
 if (!tracker.ok || tracker.status !== 200)
-  throw new Error("Failed to get verison from tracker!");
+  throw new Error("Failed to get version from tracker!");
 
 const alpha = (await tracker.json()).latest.alpha.toString();
 
