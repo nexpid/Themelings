c28d0f44fa5174e5e9e70565da52b04892cbb518
nexpid <60316309+nexpid@users.noreply.github.com>
2024-09-14 17:21:45 +0200

    fix(tasks): use py instead of python3

--- src/update/tasks/decompile.ts
+++ src/update/tasks/decompile.ts
@@ -1,54 +1,51 @@
-import { join } from "path";
 import { exists } from "node:fs/promises";
-import { handleShellErr, type Progress } from "../util";
+import { type Progress, handleShellErr, join } from "../util";
 
 const gzipWorkerURL = new URL("decompile-gzip.ts", import.meta.url).href;
 
 export default async function decompile(
-  progress: Progress,
-  pathToBundle: string,
-  tmpDir: string
+	progress: Progress,
+	pathToBundle: string,
+	tmpDir: string,
 ) {
-  const pathToDecompiler = join(tmpDir, "decompiler");
+	const pathToDecompiler = join(tmpDir, "decompiler");
 
-  progress.start("decompile_downloading");
-  if (!(await exists(pathToDecompiler))) {
-    await Bun.$`git clone https://github.com/P1sec/hermes-dec.git ${pathToDecompiler}`
-      .quiet()
-      .nothrow()
-      .then(handleShellErr);
-    progress.update("decompile_downloading", true);
-  } else progress.update("decompile_downloading", null);
+	progress.start("decompile_downloading");
+	if (!(await exists(pathToDecompiler))) {
+		await Bun.$`git clone https://github.com/P1sec/hermes-dec.git ${pathToDecompiler}`
+			.quiet()
+			.nothrow()
+			.then(handleShellErr);
+		progress.update("decompile_downloading", true);
+	} else progress.update("decompile_downloading", null);
 
-  const { exitCode: hasPython } = await Bun.$`python3 --version`
-    .nothrow()
-    .quiet();
-  if (hasPython !== 0)
-    throw new Error("Cannot use Python! Are you sure it's installed?");
+	const { exitCode: hasPython } = await Bun.$`py --version`.nothrow().quiet();
+	if (hasPython !== 0)
+		throw new Error("Cannot use Python! Are you sure it's installed?");
 
-  const pathToJs = join(tmpDir, "code.js");
+	const pathToJs = join(tmpDir, "code.js");
 
-  progress.start("decompile_decompiling");
-  if (!(await Bun.file(pathToJs).exists())) {
-    await Bun.$`python3 ${join(
-      pathToDecompiler,
-      "hbc_decompiler.py"
-    )} ${pathToBundle} ${pathToJs}`
-      .quiet()
-      .nothrow()
-      .then(handleShellErr);
-    progress.update("decompile_decompiling", true);
-  } else progress.update("decompile_decompiling", null);
+	progress.start("decompile_decompiling");
+	if (!(await Bun.file(pathToJs).exists())) {
+		await Bun.$`py ${join(
+			pathToDecompiler,
+			"hbc_decompiler.py",
+		)} ${pathToBundle} ${pathToJs}`
+			.quiet()
+			.nothrow()
+			.then(handleShellErr);
+		progress.update("decompile_decompiling", true);
+	} else progress.update("decompile_decompiling", null);
 
-  // "optional" step
+	// "optional" step
 
-  const gzipper = new Worker(gzipWorkerURL);
-  progress.start("decompile_gzip");
-  gzipper.addEventListener("message", ({ data }) => {
-    if (data === true) progress.update("decompile_gzip", true);
-    gzipper.terminate();
-  });
-  gzipper.postMessage({ path: pathToJs, target: "../data/code.gzipped.js" });
+	const gzipper = new Worker(gzipWorkerURL);
+	progress.start("decompile_gzip");
+	gzipper.addEventListener("message", ({ data }) => {
+		if (data === true) progress.update("decompile_gzip", true);
+		gzipper.terminate();
+	});
+	gzipper.postMessage({ path: pathToJs, target: "../data/code.gzipped.js" });
 
-  return pathToJs;
+	return pathToJs;
 }
