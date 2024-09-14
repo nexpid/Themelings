64abb487355dad5127b438afa24265544d715538
nexpid <60316309+nexpid@users.noreply.github.com>
2024-09-14 17:23:25 +0200

    feat(diffs): only trigger webhooks if any changes were made

--- src/update/tasks/webhook.ts
+++ src/update/tasks/webhook.ts
@@ -1,152 +1,164 @@
 import type { Canvas } from "skia-canvas";
 import draw, { convertDiffs } from "../../canvas";
-import { DiffEnum, type Diff, type OutDiffs } from "../../types";
+import { type Diff, DiffEnum, type OutDiffs } from "../../types";
 import { maxChangesThreshold } from "../util";
 
 const cap = (arr: string[], stuff: string) =>
-  arr.length > maxChangesThreshold
-    ? [
-        ...arr.slice(0, maxChangesThreshold),
-        `(+${arr.length - maxChangesThreshold} ${stuff}${
-          arr.length - maxChangesThreshold > 1 ? "s" : ""
-        })`,
-      ]
-    : arr;
+	arr.length > maxChangesThreshold
+		? [
+				...arr.slice(0, maxChangesThreshold),
+				`(+${arr.length - maxChangesThreshold} ${stuff}${
+					arr.length - maxChangesThreshold > 1 ? "s" : ""
+				})`,
+			]
+		: arr;
 
 const formatDiff = (diffs: Map<string, Diff>) => {
-  const entries = [...diffs.entries()].map(([k, v]) => ({
-    name: k,
-    ...v,
-  })) as any[];
+	const entries = [...diffs.entries()].map(([k, v]) => ({
+		name: k,
+		...v,
+	})) as any[];
 
-  const sections = {
-    Added: cap(
-      entries
-        .filter((x) => x.change === DiffEnum.Added)
-        .map((x) => `+ ${x.name}: ${x.cur}`),
-      "addition"
-    ),
-    Changed: cap(
-      entries
-        .filter((x) => x.change === DiffEnum.Changed)
-        .map((x) => `- ${x.name}: ${x.old}\n+ ${x.name}: ${x.cur}`),
-      "change"
-    ),
-    Removed: cap(
-      entries
-        .filter((x) => x.change === DiffEnum.Removed)
-        .map((x) => `- ${x.name}`),
-      "removal"
-    ),
-  };
+	const sections = {
+		Added: cap(
+			entries
+				.filter((x) => x.change === DiffEnum.Added)
+				.map((x) => `+ ${x.name}: ${x.cur}`),
+			"addition",
+		),
+		Changed: cap(
+			entries
+				.filter((x) => x.change === DiffEnum.Changed)
+				.map((x) => `- ${x.name}: ${x.old}\n+ ${x.name}: ${x.cur}`),
+			"change",
+		),
+		Renamed: cap(
+			entries
+				.filter((x) => x.change === DiffEnum.Renamed)
+				.map((x) => `- ${x.old}\n+ ${x.name}`),
+			"rename",
+		),
+		Removed: cap(
+			entries
+				.filter((x) => x.change === DiffEnum.Removed)
+				.map((x) => `- ${x.name}`),
+			"removal",
+		),
+	};
 
-  return Object.entries(sections)
-    .filter(([_, body]) => body.length > 0)
-    .map(
-      ([title, body]) => `**${title}**\n\`\`\`diff\n${body.join("\n")}\`\`\``
-    )
-    .join("\n");
+	return Object.entries(sections)
+		.filter(([_, body]) => body.length > 0)
+		.map(
+			([title, body]) => `**${title}**\n\`\`\`diff\n${body.join("\n")}\`\`\``,
+		)
+		.join("\n");
 };
 
 const formatVersion = (version: string) => {
-  const major = version.slice(0, -3);
-  const sub = version[3];
-  const min = version.slice(-2);
+	const major = version.slice(0, -3);
+	const sub = version[3];
+	const min = version.slice(-2);
 
-  return `${
-    ["Stable", "Beta", "Alpha"][Number(sub)] ?? "Unknown"
-  } ${major}.${Number(min)}`;
+	return `${
+		["Stable", "Beta", "Alpha"][Number(sub)] ?? "Unknown"
+	} ${major}.${Number(min)}`;
 };
 
 const triggerWebhook = async (
-  webhook: string,
-  {
-    role,
-    version,
-    embeds,
-  }: {
-    role?: string;
-    version: string;
-    embeds: { title: string; body: string; image?: Canvas }[];
-  }
+	webhook: string,
+	{
+		role,
+		version,
+		embeds,
+	}: {
+		role?: string;
+		version: string;
+		embeds: { title: string; body: string; image?: Canvas }[];
+	},
 ) => {
-  const images = embeds.filter((x) => x.image).map((x) => x.image);
+	const images = embeds.filter((x) => x.image).map((x) => x.image);
 
-  const formData = new FormData();
+	const formData = new FormData();
 
-  for (let i = 0; i < images.length; i++) {
-    const img = await images[i]!.toBuffer("png");
-    formData.append(
-      `files[${i}]`,
-      new Blob([img], { type: "image/png" }),
-      `${i}.png`
-    );
-  }
+	for (let i = 0; i < images.length; i++) {
+		const img = await images[i]?.toBuffer("png");
+		formData.append(
+			`files[${i}]`,
+			new Blob([img as any], { type: "image/png" }),
+			`${i}.png`,
+		);
+	}
 
-  formData.append(
-    "payload_json",
-    JSON.stringify({
-      content: role ? `<@&${role}>` : null,
-      embeds: embeds.map(({ title, body, image }) => ({
-        title,
-        description: body,
-        color: null,
-        author: {
-          name: `${version} (${formatVersion(version)})`,
-        },
-        image: image && {
-          url: `attachment://${images.indexOf(image)}.png`,
-        },
-      })),
-      allowed_mentions:
-        process.env.NODE_ENV === "test" ? { parse: [] } : { roles: [role] },
-    })
-  );
+	formData.append(
+		"payload_json",
+		JSON.stringify({
+			content: role ? `<@&${role}>` : null,
+			embeds: embeds.map(({ title, body, image }) => ({
+				title,
+				description: body,
+				color: null,
+				author: {
+					name: `${version} (${formatVersion(version)})`,
+				},
+				image: image && {
+					url: `attachment://${images.indexOf(image)}.png`,
+				},
+			})),
+			allowed_mentions:
+				process.env.NODE_ENV === "test" ? { parse: [] } : { roles: [role] },
+		}),
+	);
 
-  const res = await fetch(webhook, {
-    method: "POST",
-    body: formData,
-  });
+	const res = await fetch(webhook, {
+		method: "POST",
+		body: formData,
+	});
 
-  if (!res.ok)
-    throw new Error(
-      `Failed to send webhook message with embeds ${embeds
-        .map((x) => x.title)
-        .join(", ")}: ${await res.text()}`
-    );
+	if (!res.ok)
+		throw new Error(
+			`Failed to send webhook message with embeds ${embeds
+				.map((x) => x.title)
+				.join(", ")}: ${await res.text()}`,
+		);
 };
 
 export async function webhook(version: string, diffs: OutDiffs) {
-  await triggerWebhook(process.env.color_webhook!, {
-    role: (diffs.raw || diffs.semantic) && "1227327297795657850",
-    version,
-    embeds: [
-      {
-        title: "Raw colors",
-        body: diffs.raw ? formatDiff(diffs.raw) : "No changes",
-        image: diffs.raw
-          ? await draw(convertDiffs(diffs.raw, true))
-          : undefined,
-      },
-      {
-        title: "Semantic colors",
-        body: diffs.semantic ? formatDiff(diffs.semantic) : "No changes",
-        image: diffs.semantic
-          ? await draw(convertDiffs(diffs.semantic, true))
-          : undefined,
-      },
-    ],
-  });
+	if (diffs.raw || diffs.semantic)
+		await triggerWebhook(process.env.color_webhook!, {
+			role: "1227327297795657850",
+			version,
+			embeds: [
+				...(diffs.raw
+					? [
+							{
+								title: "Raw colors",
+								body: formatDiff(diffs.raw),
+								image: await draw(convertDiffs(diffs.raw, true)),
+							},
+						]
+					: []),
+				...(diffs.semantic
+					? [
+							{
+								title: "Semantic colors",
+								body: formatDiff(diffs.semantic),
+								image: await draw(convertDiffs(diffs.semantic, true)),
+							},
+						]
+					: []),
+			],
+		});
 
-  await triggerWebhook(process.env.icons_webhook!, {
-    role: diffs.icons && "1227327765079003217",
-    version,
-    embeds: [
-      {
-        title: "Icons",
-        body: diffs.icons ? formatDiff(diffs.icons) : "No changes",
-        image: diffs.icons ? await draw(convertDiffs(diffs.icons)) : undefined,
-      },
-    ],
-  });
+	if (diffs.icons)
+		await triggerWebhook(process.env.icons_webhook!, {
+			role: "1227327765079003217",
+			version,
+			embeds: [
+				{
+					title: "Icons",
+					body: formatDiff(diffs.icons),
+					image: await draw(convertDiffs(diffs.icons)),
+				},
+			],
+		});
 }
